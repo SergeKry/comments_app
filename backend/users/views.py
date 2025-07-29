3e6e1d4925a33cc405324cd0f43a89ff64f0323a
SergeKryv <krivosheevsrg@gmail.com>
2025-07-29 22:02:02 +0200

    /me protected endpoint

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,16 @@
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserSerializer
 
 class RegisterView(generics.CreateAPIView):
-    permission_classes = [AllowAny]
+    permission_classes = [permissions.AllowAny]
     serializer_class = RegisterSerializer
+
+
+class MeView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
