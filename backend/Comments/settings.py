c43bcba95e96251698ffaf1d752d39cc4994d003
SergeKryv <krivosheevsrg@gmail.com>
2025-07-31 22:32:18 +0200

    backend settings to enable S3 file storage

--- backend/Comments/settings.py
+++ backend/Comments/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     'drf_yasg',
     'corsheaders',
     'whitenoise',
+    'storages',
     'channels',
     'users',
     'posts',
+    'attachments',
 ]
 
 MIDDLEWARE = [
@@ -199,3 +201,11 @@ CHANNEL_LAYERS = {
     },
   }
 }
+
+# Storage settings for AWS S3
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')
---
+++ backend/attachments/models.py
@@ -0,0 +1,43 @@
+from io import BytesIO
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
+from django.core.files.base import ContentFile
+from PIL import Image
+
+ALLOWED_EXT = ('jpg','jpeg','gif','png','txt')
+MAX_TXT_BYTES = 100 * 1024
+MAX_FILES_PER_OBJ = 3
+MAX_IMAGE_SIZE = (320, 240)
+
+def validate_file(f):
+    ext = f.name.rsplit('.', 1)[-1].lower()
+    if ext not in ALLOWED_EXT:
+        raise ValidationError(f'Unsupported extension “.{ext}”')
+    if ext == 'txt' and f.size > MAX_TXT_BYTES:
+        raise ValidationError('Text files must be ≤100 KB.')
+
+class Attachment(models.Model):
+    ''' Generic attachment model to be used in Posts and Replies '''
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id    = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type','object_id')
+
+    file = models.FileField(upload_to='attachments/', validators=[validate_file])
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        ''' Resize image if it's larger than MAX_IMAGE_SIZE '''
+        ext = self.file.name.rsplit('.',1)[-1].lower()
+        if ext in ('jpg','jpeg','gif','png'):
+            img = Image.open(self.file)
+            if img.width > MAX_IMAGE_SIZE[0] or img.height > MAX_IMAGE_SIZE[1]:
+                img.thumbnail(MAX_IMAGE_SIZE, Image.ANTIALIAS)
+                buffer = BytesIO()
+                img.save(buffer, format=img.format, quality=85)
+                buffer.seek(0)
+                self.file.save(self.file.name,
+                               ContentFile(buffer.read()),
+                               save=False)
+        super().save(*args, **kwargs)
